0dec11ebb2022e50ac17c95787c3c7eeea7cd7db
Arnoldm21 <160316563+Arnoldm21@users.noreply.github.com>
2024-04-03 13:56:03 +0300

    Add files via upload

--- about.php
+++ about.php
@@ -45,12 +45,7 @@ if(isset($_SESSION['user_id'])){
 <section class="about">
 
    <div class="row">
-
-      <div class="image">
-         <img src= alt="">
-      </div>
-
-      <div class="content">
+    <div class="content">
          <h3>why choose us?</h3>
          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deserunt, neque debitis incidunt qui ipsum sed doloremque a molestiae in veritatis ullam similique sunt aliquam dolores dolore? Quasi atque debitis nobis!</p>
          <a href="menu.php" class="btn">our menu</a>
@@ -90,105 +85,13 @@ if(isset($_SESSION['user_id'])){
 </section>
 
 <!-- steps section ends -->
-
-<!-- reviews section starts  -->
-
-<section class="reviews">
-
-   <h1 class="title">customer's reivews</h1>
-
-   <div class="swiper reviews-slider">
-
-      <div class="swiper-wrapper">
-
-         <div class="swiper-slide slide">
-            <img src="images/user-icon.png" alt="">
-            <p><Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos voluptate eligendi laborum molestias ut earum nulla sint voluptatum labore nemo.</p>
-            <div class="stars">
-               <i class="fas fa-star"></i>
-               <i class="fas fa-star"></i>
-               <i class="fas fa-star"></i>
-               <i class="fas fa-star"></i>
-               <i class="fas fa-star-half-alt"></i>
-            </div>
-            <h3>Customer(i)</h3>
-         </div>
-
-         <div class="swiper-slide slide">
-            <img src="images/user-icon.png" alt="">
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos voluptate eligendi laborum molestias ut earum nulla sint voluptatum labore nemo.</p>
-            <div class="stars">
-               <i class="fas fa-star"></i>
-               <i class="fas fa-star"></i>
-               <i class="fas fa-star"></i>
-               <i class="fas fa-star"></i>
-               <i class="fas fa-star-half-alt"></i>
-            </div>
-            <h3>Customer (ii)</h3>
-         </div>
-
-         <div class="swiper-slide slide">
-            <img src="images/user-icon.png" alt="">
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos voluptate eligendi laborum molestias ut earum nulla sint voluptatum labore nemo.</p>
-            <div class="stars">
-               <i class="fas fa-star"></i>
-               <i class="fas fa-star"></i>
-               <i class="fas fa-star"></i>
-               <i class="fas fa-star"></i>
-               <i class="fas fa-star-half-alt"></i>
-            </div>
-            <h3>Customer (iii)</h3>
-         </div>
-
-         
-
-        
-         
-
-      </div>
-
-      <div class="swiper-pagination"></div>
-
-   </div>
-
-</section>
-
-<!-- reviews section ends -->
-
-
-<!-- footer section starts  -->
 <?php include 'components/footer.php'; ?>
-<!-- footer section ends -->=
 
-<script src="https://unpkg.com/swiper@8/swiper-bundle.min.js"></script>
+
+
 
 <!-- custom js file link  -->
 <script src="js/script.js"></script>
-
-<script>
-
-var swiper = new Swiper(".reviews-slider", {
-   loop:true,
-   grabCursor: true,
-   spaceBetween: 20,
-   pagination: {
-      el: ".swiper-pagination",
-      clickable:true,
-   },
-   breakpoints: {
-      0: {
-      slidesPerView: 1,
-      },
-      700: {
-      slidesPerView: 2,
-      },
-      1024: {
-      slidesPerView: 3,
-      },
-   },
-});
-
-</script>
 
 </body>
 </html>
